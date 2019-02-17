bedb93869085be2f00fe3325f84b26a66ca9329e
Ankit Kathuria <ankitkathuria@Ankits-Air.fritz.box>
2019-02-18 02:26:14 +0530

    Final Commit . Remove Prints

--- main.go
+++ main.go
@@ -16,13 +16,10 @@ var gainers map[string]interface{}
 var losers map[string]interface{}
 
 func getDataFromUrl(url string, result *map[string]interface{}) {
-    fmt.Println("__+++______HERE 1")
     response, err := http.Get(url)
-    fmt.Println("__+++______HERE 2")
 
     if err != nil {
         log.Fatal(err)
-        fmt.Println("__+++______",err)
     }
     defer response.Body.Close()
 
@@ -31,13 +28,11 @@ func getDataFromUrl(url string, result *map[string]interface{}) {
 
     // Find and print image URLs
     bs := string(body)
- 
     json.Unmarshal([]byte(bs), result)
 }
 
 func refreshGainersLosers() {
    for true {
-        fmt.Println("__+++______HERE 0")
         getDataFromUrl("https://www.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json", &losers)
         getDataFromUrl("https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json", &gainers)
         time.Sleep(300*time.Second)
@@ -48,11 +43,10 @@ func mainView(w http.ResponseWriter, r *http.Request) {
     t,err := template.ParseFiles("main.html")
     if err != nil {
         fmt.Fprint(w, "ERROR !!!!! COME BACK LATER :)")
-        fmt.Println(err)
+        log.Fatal(err)
         return
     }
     temp_args := map[string]interface{}{ "NIFTY 50 GAINERS" : gainers["data"], "NIFTY 50 LOSERS": losers["data"]}
-    fmt.Println(temp_args)
     t.Execute(w, temp_args)
 }
 
@@ -64,6 +58,5 @@ func main() {
         port = "5000"
     }
     port =":"+port
-    fmt.Println("______",port)
     log.Fatal(http.ListenAndServe(port, nil))
 }
